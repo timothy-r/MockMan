ccfc45c36377fc78f6101b9398a57a4bf16051a6
Tim Rodger <tim.rodger@gmail.com>
2014-03-16 12:09:30 +0000

    A passing implementation

--- examples/index.js
+++ examples/index.js
@@ -3,23 +3,22 @@ var assert = require('assert'),
 	mockery = require('mockery'),
     mockman = require('../lib/mockman');
 
-sut = function(date_time) {
-    return date_time.toString();
-}
-
 describe('MockMan example one', function() {
-    beforeEach(function() {
-    });
-
+    
     describe('Stub a method with a dummy object', function(){
 
          it('creating a dummy object should work for SUT', function() {
+            
+            var sut = function(dt) { 
+                return dt.call().toString();
+            };
+
             // create Mock DateTime object with method toString()
             var dt = 'Saturday 14th March 2014, 9pm';
             var date_time = mockman.instance('DateTime').shouldReceive('toString').any().willReturn(dt).getMock();
-        
+
             var result = sut(date_time);
-            console.log(result);
+            assert(result == dt);
 
             // clean up
             mockman.close();
